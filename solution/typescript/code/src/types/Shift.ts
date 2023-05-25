a7b5e417a24e1c1906b088c28275c4d0bc8a46d7
mag-e-williams <margretwilliams42@gmail.com>
2023-05-25 00:04:07 -0400

    add implementation notes

--- solution/typescript/code/src/types/Shift.ts
+++ solution/typescript/code/src/types/Shift.ts
@@ -1,9 +1,6 @@
 export type Shift = {
     name: string,
     day: string,
-    // length: '10h',
-    // startTime: '8am',
-    // endTime: '6pm',
     requiresArmedGuard: boolean,
     date: string,
 }
